326416593a1a026b33d91ba4d7728afe80d9cb52
Maksym Barvinskyi <maksym@mbarvinskyi.com>
2020-02-17 22:34:25 +0200

    added Step annotation

---
+++ automated-tests/src/test/java/com/example/annotations/Step.java
@@ -0,0 +1,11 @@
+package com.example.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target(METHOD)
+public @interface Step {
+}
